71eed900c3961c07d302ba4c548eeb9207a360ae
Nicholas <berubenic@gmail.com>
2024-11-27 18:51:36 -0500

    init

---
+++ src/main/java/com/example/ms_vehichle_total_price/MsVehicleTotalPriceApplication.java
@@ -0,0 +1,13 @@
+package com.example.ms_vehichle_total_price;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsVehicleTotalPriceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsVehicleTotalPriceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ms_vehichle_total_price/MsVehicleTotalPriceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ms_vehichle_total_price;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsVehicleTotalPriceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
